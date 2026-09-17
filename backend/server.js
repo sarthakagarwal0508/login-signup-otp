a103adee07f9f998d2829ed24eef8fb0aa4d52ed
sarthakagarwal8737-ctrl <sarthakagarwal8737@gmail.com>
2026-09-18 01:39:29 +0530

    change server for frontend and add login page

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,33 @@
+const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 
-const express = require("express");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const errorHandler = require("./middleware/errorMiddleware");
 
 const app = express();
 
-
 // Middleware
 app.use(express.json());
-
 
 // Database
 connectDB();
 
-
-// Routes
+// API Routes
 app.use("/api/auth", authRoutes);
 
+// Serve frontend
+app.use(express.static(path.join(__dirname, "../frontend")));
 
-// Home
 app.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "SecureAuth API is running"
-    });
+    res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
-
 
 // Error Handler
 app.use(errorHandler);
-
 
 const PORT = process.env.PORT || 5000;
 
